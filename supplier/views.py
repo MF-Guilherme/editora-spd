ebb999bf65c41e01e127d1de499b692db743449d
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-04-02 14:10:24 -0300

    feat(suppliers): add class and index route

--- supplier/views.py
+++ supplier/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index(request):
+    return HttpResponse('Supplier index window')
+
 def register(request):
-    return HttpResponse('Registration window')
+    return HttpResponse('Supplier register window')
