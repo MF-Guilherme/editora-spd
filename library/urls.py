a7ae5ed16bee988d82167e950d726974317293d7
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-07-18 19:44:10 -0300

    feat: author update

--- library/urls.py
+++ library/urls.py
@@ -6,5 +6,8 @@ app_name = 'library'
 urlpatterns = [
     path('book_register/', views.book_register, name='book_register'),
     path('author_register/', views.author_register, name='author_register'),
+    path('author_register/update/<int:author_id>',
+         views.author_update,
+         name='author_update'),
     path('', views.library_index, name='index')
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Book, Author
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def library_index(request):
@@ -21,4 +21,17 @@ def author_register(request):
         author = Author(name=name)
         author.save()
         messages.success(request, "Autor cadastrado com sucesso!")
+        return redirect('/library/author_register')
+
+
+def author_update(request, author_id):
+    if request.method == 'GET':
+        author = Author.objects.filter(id=author_id).first()
+        return render(request, 'author_update.html', {'author': author})
+    elif request.method == 'POST':
+        author = Author.objects.filter(id=author_id).first()
+        new_author = request.POST.get('author-name')
+        author.name = new_author
+        author.save()
+        messages.success(request, 'Nome do autor atualizado com sucesso!')
         return redirect('/library/author_register')
